0210f772f873462c3ecc589e6cd2aa5f1099047d
Techin <techin.pw@gmail.com>
2018-12-28 00:50:03 +0700

    complete login with Facebook

--- TodoList/LoginViewController.swift
+++ TodoList/LoginViewController.swift
@@ -11,17 +11,34 @@ import FBSDKCoreKit
 import FBSDKLoginKit
 import Firebase
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, FBSDKLoginButtonDelegate{
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
 //        createFBbutton()
+        _createbuttonFB()
         
     }
+    
+    func _createbuttonFB() {
+        let loginbuttonFB = FBSDKLoginButton()
+        loginbuttonFB.frame = CGRect(x: 16, y: 50, width: view.frame.width - 32 , height: 50)
+        loginbuttonFB.delegate = self
+        view.addSubview(loginbuttonFB)
+        loginbuttonFB.readPermissions = ["email", "public_profile"]
+    }
+    
    
     @IBAction func loginFBButton(_ sender: Any) {
-        
+        FBSDKLoginManager().logIn(withReadPermissions: ["email" , "public_profile"], from: self) { (result, error) in
+            if error != nil {
+                print("Button Login Failed : \(String(describing: error))")
+                return
+            }
+            self.showEmailFB()
+        }
     }
         
 //        let loginFBManager = FBSDKLoginManager()
@@ -93,6 +110,39 @@ class LoginViewController: UIViewController {
 //        })
 //    }
     
+    func showEmailFB() {
+        let accessTokenCurrent = FBSDKAccessToken.current()
+        guard let accessTokenString = accessTokenCurrent?.tokenString else {return}
+        
+        let credential = FacebookAuthProvider.credential(withAccessToken: accessTokenString)
+            
+        Auth.auth().signInAndRetrieveData(with: credential, completion: { (result, error) in
+            if error != nil {
+                print("Something wrong")
+                }
+            else {
+                print("Successfullt logged in \(String(describing: result))")
+                self.performSegue(withIdentifier: "afterloginFB", sender: nil)
+                }
+            })
+        FBSDKGraphRequest(graphPath: "/me", parameters: ["fields": "id, name, email"])?.start(completionHandler: { (connection, result, error) in
+            if error != nil {
+                print("Failed to start graph request: \(String(describing: error))")
+                return
+            }
+            print(result)
+        })
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil {
+            print(error)
+            return
+        }
+        
+        showEmailFB()
+        
+    }
 
     
     func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
